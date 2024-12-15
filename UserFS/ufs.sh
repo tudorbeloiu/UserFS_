564e5d19b36d63709d6f75955c8bd37d14048563
tudorbeloiu <beloiutudorandrei@gmail.com>
2024-12-15 16:35:46 +0200

    Finished project

--- UserFS/ufs.sh
+++ UserFS/ufs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-root_dir="/$HOME/rootDirectory"
-raport_zilnic="$rootdir/raport.txt"
+root_dir="$HOME/rootDirectory"
+raport="$HOME/dailyReport"
 
 read -p "Numarul de procese cele mai active: " num
 
@@ -66,31 +66,33 @@ adaugare_user(){
 }
 
 generare_raport(){
+   mkdir -p "$raport"
+   echo "boyna galava" > "$raport/raport_zilnic"
    contor=0
-   utilizatori_activi=$(who | awk '${print $1}' | sort | uniq)
+   utilizatori_activi=$(who | awk '{print $1}' | sort | uniq)
    for activ in "$utilizatori_activi"
    do
      ((contor++))
    done
    #echo $contor
-   most_procese_utilizate=$(ps aux | awk '${print $11}' | sort | uniq -c | sort -nr | head -n $num)
-   most_utilizatori_activi=$(ps aux | awk '${print $1}' | sort | uniq -c | sort -nr | head -n $num)
+   most_procese_utilizate=$(ps aux | awk '{print $11}' | sort | uniq -c | sort -nr | head -n $num)
+   most_utilizatori_activi=$(ps aux | awk '{print $1}' | sort | uniq -c | sort -nr | head -n $num)
 
-   echo "Raportul UserFS in data de: $(date)" > "$raport_zilnic"
-   echo "---------------------------------------"
-   echo "Numarul total de utilizatori activi astazi: $contor" > "$raport_zilnic"
-   echo "Utilizatorii activi astazi: " > "$raport_zilnic"
-   echo "$utilizatori_activi" > "$raport_zilnic"
-   echo "---------------------------------------"
-   echo "Cele mai utilizate procese astazi: " > "$raport_zilnic"
-   echo "$most_procese_utilizate" > "$raport_zilnic"
-   echo "---------------------------------------"
-   echo "Cei mai activi utilizatori astazi: " > "$raport_zilnic"
-   echo "$most_utilizatori_activi" > "$raport_zilnic"
-   echo "---------------------------------------"
-   echo "---------------------------------------"
+   echo "Raportul UserFS in data de: $(date)" > "$raport/raport_zilnic"
+   echo "---------------------------------------" >> "$raport/raport_zilnic"
+   echo "Numarul total de utilizatori activi astazi: $contor" >> "$raport/raport_zilnic"
+   echo "Utilizatorii activi astazi: " >> "$raport/raport_zilnic"
+   echo "$utilizatori_activi" >> "$raport/raport_zilnic"
+   echo "---------------------------------------" >> "$raport/raport_zilnic"
+   echo "Cele mai utilizate procese astazi: " >> "$raport/raport_zilnic"
+   echo "$most_procese_utilizate" >> "$raport/raport_zilnic"
+   echo "---------------------------------------" >> "$raport/raport_zilnic"
+   echo "Cei mai activi utilizatori astazi: " >> "$raport/raport_zilnic"
+   echo "$most_utilizatori_activi" >> "$raport/raport_zilnic"
+   echo "---------------------------------------" >> "$raport/raport_zilnic"
+   echo "---------------------------------------" >> "$raport/raport_zilnic"
 
-   echo "Utilizatori suspiciosi: " > "$raport_zilnic"
+   echo "Utilizatori suspiciosi: " >> "$raport/raport_zilnic"
 
    for user_directory in "$root_dir"/*
    do
@@ -100,28 +102,27 @@ generare_raport(){
         cnt_procese_user=$(cat "$user_directory/procs" | wc -l)
         if [[ "$cnt_procese_user" -gt "$numproc" ]]
         then
-          echo "$utilizator are $cnt_procese_user active" > "$raport_zilnic"
+          echo "$utilizator are $cnt_procese_user procese active" >> "$raport/raport_zilnic"
         fi
       fi
    done
 }
 
-timp_reactualizare=30
 
 while true
 do
   adaugare_user
-  sleep "$timp_reactualizare"
+  sleep 30
 done &
 
 
 while true
 do
   ora=$(date +"%H:%M")
-  if [[ "$ora" == "00:00" ]]
+  if [[ "$ora" == "16:35" ]]
   then
     generare_raport
     sleep 60
   fi
-  sleep 30
-done
+  sleep 10
+done &
